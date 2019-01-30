2c3efae16ad85d0ea6bc8437b2d42260e98b1252
gurjotsingh4398 <gurjotsingh4398@gmail.com>
2019-01-30 14:46:51 +0530

    setup basic client app

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+serviceWorker.unregister();
